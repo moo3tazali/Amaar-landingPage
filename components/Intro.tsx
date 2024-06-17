41a7041f4c176654c3251b74a34e4093ae4f5913
moo3tazali <moo3tazali@gmail.com>
2024-06-18 01:15:59 +0300

    fix issue

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,66 +1,54 @@
-"use client";
-import Image from "next/image";
-import { motion, useScroll, useTransform } from "framer-motion";
+'use client';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
 
-import introImage from "@/public/intro.png";
-import { AnimatedText } from "./AnimatedText";
-import { useCurrentLocale, useI18n } from "@/locales/client";
-import { useRef } from "react";
-import { SwipeCarousel } from "./SwipeCarousel";
+import introImage from '@/public/intro.png';
+import { useI18n } from '@/locales/client';
 
 const Intro = () => {
-  const locale = useCurrentLocale();
   const t = useI18n();
-  // const targetRef = useRef(null);
-  // const { scrollYProgress } = useScroll({
-  //   target: targetRef,
-  //   offset: ["end end", "end start"],
-  // });
-
-  // const x = useTransform(scrollYProgress, [0, 1], ["50%", "0%"]);
-  // const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
 
   return (
-    <section className="intro-h flex flex-row-reverse overflow-hidden">
-      <div className="flex-1 flex flex-col justify-center items-center gap-7 intro-h text-center w-full md:w-1/2 rtl:mr-auto">
+    <section className='intro-h flex flex-row-reverse overflow-hidden'>
+      <div className='flex-1 flex flex-col justify-center items-center gap-7 intro-h text-center w-full md:w-1/2 rtl:mr-auto'>
         <motion.h1
-          className="uppercase text-6xl font-semibold leading-tight max-w-[350px]"
+          className='uppercase text-6xl font-semibold leading-tight max-w-[350px]'
           initial={{ scale: 0, opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
           transition={{
             duration: 0.8,
-            type: "spring",
+            type: 'spring',
           }}
         >
-          {t("intro_text")}
+          {t('intro_text')}
         </motion.h1>
 
         <motion.p
-          className="text-2xl dark:text-slate-400 text-gray-600"
+          className='text-2xl dark:text-slate-400 text-gray-600'
           initial={{ y: 300, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.8, type: "spring" }}
+          transition={{ duration: 0.8, type: 'spring' }}
         >
-          {t("from")}{" "}
-          <span className=" capitalize underline">
-            {t("intro_underline_text")}
+          {t('from')}{' '}
+          <span className=' capitalize underline'>
+            {t('intro_underline_text')}
           </span>
         </motion.p>
       </div>
 
-      <div className="flex-1 overflow-hidden hidden md:block">
+      <div className='flex-1 overflow-hidden hidden md:block'>
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{
             duration: 0.8,
-            type: "spring",
+            type: 'spring',
           }}
         >
           <Image
             src={introImage}
-            alt="intro-image"
-            className="absolute -z-10 left-[12px] top-[133px] overflow-hidden rounded-[35px] w-full hero-section-bg rtl:translate-x-1/2 ltr:-translate-x-1/2 object-cover h-full"
+            alt='intro-image'
+            className='absolute -z-10 left-[12px] top-[133px] overflow-hidden rounded-[35px] w-full hero-section-bg rtl:translate-x-1/2 ltr:-translate-x-1/2 object-cover h-full'
           />
         </motion.div>
       </div>
--- components/SwipeCarousel.tsx
+++ components/SwipeCarousel.tsx
@@ -1,6 +1,9 @@
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import { projectImg } from "@/lib/data";
+import React from 'react';
+import Image from 'next/image';
+import Carousel from 'react-multi-carousel';
+
+import 'react-multi-carousel/lib/styles.css';
+import { projectImg } from '@/lib/data';
 
 const responsive = {
   desktop: {
@@ -20,10 +23,7 @@ const responsive = {
   },
 };
 
-import React from "react";
-import Image from "next/image";
-
-const SwipeCarousel = () => {
+export const SwipeCarousel = () => {
   return (
     <Carousel
       swipeable={true}
@@ -35,24 +35,24 @@ const SwipeCarousel = () => {
       autoPlay={true}
       autoPlaySpeed={5000}
       keyBoardControl={true}
-      customTransition="ease-in-out"
+      customTransition='ease-in-out'
       transitionDuration={500}
-      containerClass="carousel-container"
-      removeArrowOnDeviceType={["tablet", "mobile"]}
-      itemClass="carousel-item-padding-40-px"
+      containerClass='carousel-container'
+      removeArrowOnDeviceType={['tablet', 'mobile']}
+      itemClass='carousel-item-padding-40-px'
     >
       {projectImg.map((img, i) => (
         <div
-          style={{ direction: "ltr" }}
+          style={{ direction: 'ltr' }}
           key={i}
-          className="flex flex-col gap-5 items-center justify-center"
+          className='flex flex-col gap-5 items-center justify-center'
         >
           <Image
             src={img}
-            alt="image 1"
-            className="max-h-full max-w-full object-cover md:rounded-2xl flex-1"
+            alt='image 1'
+            className='max-h-full max-w-full object-cover md:rounded-2xl flex-1'
           />
-          <p className="text-lg text-center p-5">
+          <p className='text-lg text-center p-5'>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas
             cum aliquid est laborum labore necessitatibus, odit itaque illo
             velit ipsum nesciunt corrupti ducimus numquam iste non nulla.
